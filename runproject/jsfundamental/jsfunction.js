0750c1377906e742083707e70721a8adfe41544a
nishant123 <chawla.nishant@gmail.com>
2024-10-21 10:03:35 +0500

    c

--- runproject/jsfundamental/jsfunction.js
+++ runproject/jsfundamental/jsfunction.js
@@ -5,12 +5,14 @@ let str ="hello world";
 function countInstances(string, word) {
    return string.split(word).length - 1;
 }
+
 let g=[];
 k=0;
 for(let i=0;i<str.length;i++) {
     
-    if(g.indexOf(str[i])<1){
-        
+    if(g.indexOf(str[i])<1)
+    {        
+           
     console.log(str[i]+" count is " +countInstances(str, str[i]));
     g[k] = str[i];
     k++;
